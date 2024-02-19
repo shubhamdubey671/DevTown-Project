895a6f536aa5e266e01455c06c1c13df3e6fc377
shubhamdube671 <shubhamdubey671@gmail.com>
2024-02-19 15:34:20 +0530

    Final commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     <>
       <div className=" h-screen w-full flex justify-center items-center	">
         <button
-          className=" h-auto	 w-auto	 bg-sky-500/100 p-2.5  rounded-md"
+          className=" h-auto w-auto	 bg-sky-500/100 p-2.5  rounded-md"
           onClick={handler}
         >
           Login
